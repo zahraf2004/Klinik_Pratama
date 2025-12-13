17e1c8432438a327eb674bd5fd9549412e222563
zahraf2004 <zahra.f.andiani@gmail.com>
2025-12-13 11:30:08 +0700

    19

--- resources/views/adminDataNakes/DataNakes.blade.php
+++ resources/views/adminDataNakes/DataNakes.blade.php
@@ -95,7 +95,7 @@
 </section>
 
 <!-- Modal Popup -->
-  @include('adminDataNakes._modalForm')
+  @include('adminDataNakes._modalform')
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
